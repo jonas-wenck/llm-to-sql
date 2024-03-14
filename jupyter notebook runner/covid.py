c15c9d7efcc63f692f639535892e8d3365072c42
JonasWenck <jonasw.goes@gmail.com>
2024-03-14 15:46:06 +0100

    cleaned up and unified the table DDL to hopefully increase model performance

--- jupyter notebook runner/covid.py
+++ jupyter notebook runner/covid.py
@@ -1,6 +1,5 @@
 # DDL und prompts für die Neuseeland Datenquelle
-ddl = """USE [covid_data]
-
+ddl = """
 CREATE TABLE [dbo].[covid](
 	[date] [date] NULL,
 	[state] [varchar](50) NULL,
@@ -8,12 +7,8 @@ CREATE TABLE [dbo].[covid](
 	[cases] [int] NULL,
 	[deaths] [int] NULL,
 	[id] [int] NOT NULL,
-CONSTRAINT [PK_COVID] PRIMARY KEY CLUSTERED 
-(
-	[id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-
+PRIMARY KEY CLUSTERED ([id] ASC)
+);
 
 CREATE TABLE [dbo].[us_states_population](
 	[Geographic_Area] [nvarchar](150) NULL,
@@ -23,27 +18,14 @@ CREATE TABLE [dbo].[us_states_population](
 	[20220701_Population_Estimate] [int] NULL,
 	[20230701_Population_Estimate] [int] NULL,
 	[ID] [int] NOT NULL,
-CONSTRAINT [PK_POPULATION] PRIMARY KEY CLUSTERED 
-(
-	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-
-ALTER TABLE [dbo].[us_states_population] ADD  DEFAULT (NULL) FOR [20200401_Estimates_Base]
-
-ALTER TABLE [dbo].[us_states_population] ADD  DEFAULT (NULL) FOR [20200701_Population_Estimate]
-
-ALTER TABLE [dbo].[us_states_population] ADD  DEFAULT (NULL) FOR [20210701_Population_Estimate]
-
-ALTER TABLE [dbo].[us_states_population] ADD  DEFAULT (NULL) FOR [20220701_Population_Estimate]
-
-ALTER TABLE [dbo].[us_states_population] ADD  DEFAULT (NULL) FOR [20230701_Population_Estimate]
+PRIMARY KEY CLUSTERED ([ID] ASC)
+);
 """
 
 prompts = [
-     'Which US state had the most corona deaths?'
-    ,'Which US state was the last one to have its first corona death and when was that?'
-    ,'Which US state had, relative to its population, the least amount of corona cases and what was that percentage?'
-    ,'How many daily new corona cases where there on average for each US state?'
-    ,'In which month occurred the most corona cases within a single US state and which state was that?'
+    'Which US state had the most corona deaths?'
+    , 'Which US state was the last one to have its first corona death and when was that?'
+    , 'Which US state had, relative to its population, the least amount of corona cases and what was that percentage?'
+    , 'How many daily new corona cases where there on average for each US state?'
+    , 'In which month occurred the most corona cases within a single US state and which state was that?'
 ]
